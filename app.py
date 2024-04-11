f6ac39b220dc83d001203cd1ac93ec80bfe6da84
Pratikshakulkar <136431083+Pratikshakulkar@users.noreply.github.com>
2024-04-11 18:07:20 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 22:34:20 2020
+
+@author: Krish Naik
+"""
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import tensorflow as tf
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 22:34:20 2020
+
+@author: Krish Naik
+"""
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import tensorflow as tf
